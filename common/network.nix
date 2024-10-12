e3b02bd2fb2d9f057ee1046ef641528f32b6aaf4
Pavel Stepanov <paulkreuzmann@gmail.com>
2024-10-12 13:04:37 +0600

    add my hosts

--- common/network.nix
+++ common/network.nix
@@ -5,5 +5,9 @@
     hostName = hostName;
     networkmanager.enable = true;
     firewall.enable = false;
+    hosts = {
+      "200:33c5:d35:b3bf:7ed4:2aa4:83cb:196f" = [ "omen15" ];
+      "202:9e4b:8cad:eb23:9340:178a:c29:e344" = [ "nix450s" ];
+    };
   };
 }
