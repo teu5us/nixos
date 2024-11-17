dcb52a86fb92c62dc532a93b24f829f689c00c4d
Pavel Stepanov <paulkreuzmann@gmail.com>
2024-11-17 12:02:56 +0600

    disable psr

--- machines/nix450s/base.nix
+++ machines/nix450s/base.nix
@@ -31,7 +31,7 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
 
   boot.extraModprobeConfig = ''
-    options i915 modeset=1 enable_fbc=1 enable_dc=2 enable_psr=1 fastboot=1 enable_dpcd_backlight=3 enable_guc=0 enable_gvt=1
+    options i915 modeset=1 enable_fbc=1 enable_dc=2 enable_psr=0 fastboot=1 enable_dpcd_backlight=3 enable_guc=0 enable_gvt=1
     options drm vblankoffdelay=1
     options snd_hda_intel power_save=1
     options iwlwifi power_save=1 power_level=5 swcrypto=0 uapsd_disable=0 d0i3_disable=0
