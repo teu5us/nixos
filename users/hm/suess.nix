6846dfe57e738a39c40857292e6f49c9aa8d61b9
Pavel Stepanov <paulkreuzmann@gmail.com>
2025-01-01 19:42:27 +0600

    zathura

--- users/hm/suess.nix
+++ users/hm/suess.nix
@@ -66,6 +66,7 @@ in
         enableZshIntegration = true;
       };
       keybindings = {
+        "ctrl+shift+q" = "no_op";
         "alt+s" = "toggle_layout stack";
         "alt+shift+s" = "goto_layout stack";
         "alt+shift+g" = "goto_layout grid";
@@ -216,6 +217,34 @@ in
       enableBashIntegration = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
+    };
+
+    zathura = {
+      enable = true;
+      options = {
+        "default-bg" = "#1a1b26";
+        "default-fg" = "#a9b1d6";
+        "statusbar-fg" = "#a9b1d6";
+        "statusbar-bg" = "#24283b";
+        "inputbar-bg" = "#1a1b26";
+        "inputbar-fg" = "#73daca";
+        "notification-bg" = "#1a1b26";
+        "notification-fg" = "#73daca";
+        "notification-error-bg" = "#1a1b26";
+        "notification-error-fg" = "#f7768e";
+        "notification-warning-bg" = "#1a1b26";
+        "notification-warning-fg" = "#f7768e";
+        "highlight-color" = "#e0af68";
+        "highlight-active-color" = "#9aa5ce";
+        "completion-bg" = "#24283b";
+        "completion-fg" = "#a9b1d6";
+        "completion-highlight-fg" = "#9aa5ce";
+        "completion-highlight-bg" = "24283b";
+        "recolor-lightcolor" = "#16161e";
+        "recolor-darkcolor" = "#a9b1d6";
+        "recolor" = "true";
+        "recolor-keephue" = "false";
+      };
     };
 
     zoxide = {
